8f2fa14f4fca0d32c1cd9e86bb91715cd3cec7fa
Nadine <nadine@soar.earth>
2021-11-04 14:47:24 +0800

    added gitignore and package-lock.json (late npm isntall). Modified AddUser.js

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-const AddUser = () => {
-    return 
+const AddUser = (props) => {
+    return(
+    <form>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text/>
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number/>
+    </form> 
+    );
 };
+
+export degault AddUser;
