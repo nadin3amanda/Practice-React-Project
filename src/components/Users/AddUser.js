728cc2a8c5ce7abcaf5d6f063127be640fc0ebb3
Nadine <nadine@soar.earth>
2021-11-04 14:52:40 +0800

    Fixed typographical errors

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,14 +1,14 @@
 import React from "react";
 
 const AddUser = (props) => {
-    return(
+  return (
     <form>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text/>
-        <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number/>
-    </form> 
-    );
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text" />
+      <label htmlFor="age">Age (Years)</label>
+      <input id="age" type="number" />
+    </form>
+  );
 };
 
-export degault AddUser;
+export default AddUser;
