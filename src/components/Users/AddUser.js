6a019dc25ab8080365b5763f4073965fdf408d24
Nadine <nadine@soar.earth>
2021-11-04 15:11:48 +0800

    added labels and button

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 const AddUser = (props) => {
+const addUserHandler = (event) => {
+    event.preventDefault();
+}
+
   return (
-    <form>
+    <form onSubmit= {addUserHandler}>
       <label htmlFor="username">Username</label>
       <input id="username" type="text" />
       <label htmlFor="age">Age (Years)</label>
       <input id="age" type="number" />
+      <button type="submit">Add User </button>
     </form>
   );
 };
