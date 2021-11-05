18043ae8e15cd6c87d9778f4c3fe749685b1e254
Nadine <nadine@soar.earth>
2021-11-05 17:29:41 +0800

    added new function addUserHandler and event handlers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name: uName, age: uAge }];
+    });
+  };
+
   return (
     <div>
-      <AddUser></AddUser>
-      <UsersList users={[]}></UsersList>
+      <AddUser onAddUser={addUserHandler}></AddUser>
+      <UsersList users={usersList}></UsersList>
     </div>
   );
 }
