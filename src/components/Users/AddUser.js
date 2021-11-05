ba954893a98d4a6bdf21acc53358595e572922d1
Nadine <nadine@soar.earth>
2021-11-05 13:58:03 +0800

    removed props  on OnChange under input id

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -20,21 +20,13 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-  };
-
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          onChange={props.usernameChangeHandler}
-        />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
         <label htmlFor="age"> Age (Years)</label>
-        <input id="age" type="number" onChange={props.ageChangeHandler} />
+        <input id="age" type="number" onChange={ageChangeHandler} />
         <Button type="submit">Add User </Button>
       </form>
     </Card>
