272622eff3910639608dc8869515eda1268a0b00
Nadine <nadine@soar.earth>
2021-11-08 15:07:49 +0800

    added function for errors (errorHandler)

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -39,12 +39,13 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      {error && <ErrorModal
-        title={error.title}
-        message={error.message}
-      />}
+      {error && <ErrorModal title={error.title} message={error.message} />}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
