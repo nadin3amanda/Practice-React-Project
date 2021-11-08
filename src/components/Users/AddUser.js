62885c249f4a7c76132c7521074ecc0b5ed94071
Nadine <nadine@soar.earth>
2021-11-08 14:45:50 +0800

    added class for errors and logged issue for backdrop  that is covering the cards

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -8,13 +8,22 @@ import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState("");
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age. Empty values not allowed ",
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid age (> 0).",
+      });
       return;
     }
     props.onAddUser(enteredUsername, enteredAge);
@@ -32,7 +41,10 @@ const AddUser = (props) => {
 
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Sorry, something went wrong..." />
+      {error && <ErrorModal
+        title={error.title}
+        message={error.message}
+      />}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
