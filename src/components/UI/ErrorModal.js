146bd1985b1785786d04b5f683713dc42af0b2f0
Nadine <nadine@soar.earth>
2021-11-08 12:57:33 +0800

    Added classNames for backdrop, header, content, and actions. Added div to wrap card on AddUser.js

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,20 +1,24 @@
 import React from "react";
 import Card from "./Card";
 import Button from "./Button";
+import classes from "./ErrorModal.module.css";
 
-const ErrorModel = (props) => {
+const ErrorModal = (props) => {
   return (
-    <Card>
-      <header>
-        <h2>{props.title} </h2>
-      </header>
-      <div>
-        <p>{props.message} </p>
-      </div>
-      <footer>
-        <Button></Button>
-      </footer>
-    </Card>
+    <div>
+      <div className={classes.backdrop}></div>
+      <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title} </h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.message} </p>
+        </div>
+        <footer className={classes.actions}>
+          <Button>OK</Button>
+        </footer>
+      </Card>
+    </div>
   );
 };
 
